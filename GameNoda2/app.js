d2f9a6bd4293f47face968b7477f7f2f803827d6
veloc1tymeow <trashixx775@gmail.com>
2024-05-05 18:56:35 +0300

    проект

---
+++ GameNoda2/app.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const mainRouteController = require("./controllers/main.js");
+const defaultRouteController = require("./controllers/default.js");
+const gameRouteController = require("./controllers/game.js");
+const voteRouteController = require("./controllers/vote.js");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  switch (url) {
+    case "/":
+      mainRouteController(res, "/index.html", ".html");
+      break;
+    case "/game":
+      gameRouteController(res);
+      break;
+    case "/vote":
+      voteRouteController(req, res);
+      break;
+    default:
+      defaultRouteController(res, url);
+  }
+});
+
+server.listen(3005);
